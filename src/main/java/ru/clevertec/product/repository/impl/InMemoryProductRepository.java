1c436ec2683c3b73b3d0c0cd8800e2cbd21d35d6
Aliaksei Lukyanski <alexlukyanski@gmail.com>
2023-11-01 19:35:51 +0300

    feat: implementation

--- src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
+++ src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
@@ -1,31 +1,40 @@
 package ru.clevertec.product.repository.impl;
 
+import lombok.RequiredArgsConstructor;
 import ru.clevertec.product.entity.Product;
+import ru.clevertec.product.repository.DataContainer;
 import ru.clevertec.product.repository.ProductRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class InMemoryProductRepository implements ProductRepository {
+
+    private DataContainer dataContainer = DataContainer.getInstance();
 
     @Override
     public Optional<Product> findById(UUID uuid) {
-        return Optional.empty();
+        return Optional.of(dataContainer.getMapWithData().get(uuid));
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return dataContainer.getMapWithData().values().stream()
+                .toList();
     }
 
     @Override
     public Product save(Product product) {
-        return null;
+        UUID uuid = product.getUuid();
+        dataContainer.getMapWithData().put(uuid, product);
+        return dataContainer.getMapWithData().get(uuid);
     }
 
     @Override
     public void delete(UUID uuid) {
-
+        dataContainer.getMapWithData().put(uuid, null);
     }
 }
+
