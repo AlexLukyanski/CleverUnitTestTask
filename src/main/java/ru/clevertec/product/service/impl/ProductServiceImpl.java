18f81894e8d81d15bdeb65606d9a035b1c27ed65
Aliaksei Lukyanski <alexlukyanski@gmail.com>
2023-11-01 19:35:51 +0300

    feat: implementation

--- src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
@@ -3,11 +3,13 @@ package ru.clevertec.product.service.impl;
 import lombok.RequiredArgsConstructor;
 import ru.clevertec.product.data.InfoProductDto;
 import ru.clevertec.product.data.ProductDto;
+import ru.clevertec.product.entity.Product;
 import ru.clevertec.product.mapper.ProductMapper;
 import ru.clevertec.product.repository.ProductRepository;
 import ru.clevertec.product.service.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -18,26 +20,45 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public InfoProductDto get(UUID uuid) {
-        return null;
+
+        Optional<Product> optionalProduct = productRepository.findById(uuid);
+
+        return optionalProduct.stream()
+                .map(mapper::toInfoProductDto)
+                .findFirst()
+                .orElseThrow();
     }
 
     @Override
     public List<InfoProductDto> getAll() {
-        return null;
+
+        return productRepository.findAll().stream()
+                .map(mapper::toInfoProductDto)
+                .toList();
     }
 
     @Override
     public UUID create(ProductDto productDto) {
-        return null;
+
+        Product product = mapper.toProduct(productDto);
+
+        return productRepository.save(product)
+                .getUuid();
     }
 
     @Override
     public void update(UUID uuid, ProductDto productDto) {
 
+        Product product = new Product();
+        product.setUuid(uuid);
+
+        product = mapper.merge(product, productDto);
+        productRepository.save(product);
     }
 
     @Override
     public void delete(UUID uuid) {
 
+        productRepository.delete(uuid);
     }
 }
